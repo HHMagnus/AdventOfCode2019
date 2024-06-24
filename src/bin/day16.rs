ebefced8903b0d918423d7024afcecb08a80f39f
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-06-24 15:37:59 +0200

    day 16 part 1

---
+++ src/bin/day16.rs
@@ -0,0 +1,25 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let file = read_to_string("input/day16.txt").unwrap();
+
+    let input = [0, 1, 0, -1];
+
+    let list = file.chars().map(|x| x.to_digit(10).unwrap() as i64).collect::<Vec<_>>();
+
+    let mut part1 = list.clone();
+
+    for _ in 0..100 {
+        part1 = (0..part1.len()).into_iter().map(|p| {
+            let value = part1.iter().enumerate().map(|(i, x)| input[((i+1) / (p+1)) % input.len()] * x).sum::<i64>();
+            value.to_string().chars().last().unwrap().to_digit(10).unwrap() as i64
+        }).collect();
+    }
+
+    let part1 = part1.iter().map(|x| x.to_string()).collect::<Vec<_>>().join("");
+
+    println!("Day 16 part 1: {}", part1);
+
+    // 16843933 too low
+
+}
